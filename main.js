700b9b7cbe2f9facfd002f99183bcca13266ca43
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-04-28 17:31:37 +0200

    Karte mit Startlayer und Stephansdom als Zentrum

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ let stephansdom = {
   lng: 16.373118,
   title: "Stephansdom"
 };
+
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [ stephansdom.lat, stephansdom.lng ],
+    zoom: 12,
+    layers: [
+        startLayer
+    ],
+});
+
