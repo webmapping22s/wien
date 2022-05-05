6cfb837a8f68dbf43214cb6a9d961069ad19c9b4
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-05 16:41:17 +0200

    Hotels und Unterkünfte implementiert

--- main.js
+++ main.js
@@ -111,3 +111,17 @@ async function loadZones(url) {
 }
 loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
 
+// Hotels und Unterkünfte
+async function loadHotels(url) {
+    let response = await fetch(url);    
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay);
+}
+loadHotels("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json");
+
