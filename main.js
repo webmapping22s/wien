7eacbcde4b3ba3f2b273f3cefb4735207d0be82b
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-05 16:37:51 +0200

    Fußgängerzonen implementiert

--- main.js
+++ main.js
@@ -97,3 +97,17 @@ async function loadLines(url) {
 }
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
 
+// Fußgängerzonen
+async function loadZones(url) {
+    let response = await fetch(url);    
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Fußgängerzonen");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay);
+}
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+
