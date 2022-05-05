3ecbb36ad365048ab2a01ce484d6feecc864beb1
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-05 17:08:49 +0200

    pointToLayer für Sehenswürdigkeiten verwendet

--- main.js
+++ main.js
@@ -65,7 +65,11 @@ async function loadSites(url) {
     layerControl.addOverlay(overlay, "Sehenswürdigkeiten");
     overlay.addTo(map);
 
-    L.geoJSON(geojson).addTo(overlay);
+    L.geoJSON(geojson, {
+        pointToLayer: function(geoJsonPoint, latlng) {
+            return L.marker(latlng);
+        }
+    }).addTo(overlay);
 }
 loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
 
