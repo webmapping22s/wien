1817ace1bb115bf27858867717b8fb69100a011e
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-05 16:26:14 +0200

    Haltestellen Vienna Sightseeing eingebaut

--- main.js
+++ main.js
@@ -69,3 +69,17 @@ async function loadSites(url) {
 }
 loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
 
+// Haltestellen Vienna Sightseeing
+async function loadStops(url) {
+    let response = await fetch(url);    
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Haltestellen Vienna Sightseeing");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay);
+}
+loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
