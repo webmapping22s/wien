4fb66807b69cddea912dce9fe61cf730f9211e5d
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-05 16:04:21 +0200

    asynchrone Funktion zum Laden der Sehenswürdigkeiten

--- main.js
+++ main.js
@@ -54,3 +54,11 @@ let miniMap = new L.Control.MiniMap(
         toggleDisplay: true
     }
 ).addTo(map);
+
+async function loadSites(url) {
+    let response = await fetch(url);    
+    let geojson = await response.json();
+    console.log(geojson);
+}
+loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+
