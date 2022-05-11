5ef429cda320a06e2df9a4407df5761e6da70eb3
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-11 17:11:57 +0200

    GeoJSON Kopie der Haltestellen für Hotels und Unterkünfte

--- main.js
+++ main.js
@@ -160,7 +160,23 @@ async function loadHotels(url) {
     layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
     overlay.addTo(map);
 
-    L.geoJSON(geojson).addTo(overlay);
+    L.geoJSON(geojson, {
+        pointToLayer: function(geoJsonPoint, latlng) {
+            //L.marker(latlng).addTo(map)
+            //console.log(geoJsonPoint.properties);
+            let popup = `
+                <strong>${geoJsonPoint.properties.LINE_NAME}</strong><br>
+                Station ${geoJsonPoint.properties.STAT_NAME}
+            `;
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `icons/bus_${geoJsonPoint.properties.LINE_ID}.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(overlay);
 }
 //loadHotels("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json");
 
