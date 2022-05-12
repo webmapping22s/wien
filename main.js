f0751161e79889f4acdad4f901a34292bc58e3fa
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-12 16:50:07 +0200

    alle Cluster bei zoomlevel 17 auflösen

--- main.js
+++ main.js
@@ -198,7 +198,9 @@ async function loadHotels(url) {
     let geojson = await response.json();
     //console.log(geojson);
 
-    let overlay = L.markerClusterGroup();
+    let overlay = L.markerClusterGroup({
+        disableClusteringAtZoom: 17
+    });
     layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
     overlay.addTo(map);
 
